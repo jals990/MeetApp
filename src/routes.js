d5a73fc29bfc5ff1fd23aaf3f2cf84f43dd1a1b1
Gabriel Carvalho <fale@gcarvalho.dev>
2019-10-02 21:12:08 -0300

    chore: Adding Files
    Adding files to the server system

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.post);
@@ -13,4 +16,7 @@ routes.use(authMiddleware);
 
 routes.update('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 export default routes;
